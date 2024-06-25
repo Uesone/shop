2fcdeb9a8c7276bf4b09f2b4664e8c01db1d232c
Umberto Amoroso <34135219+Uesone@users.noreply.github.com>
2024-06-25 14:55:02 +0200

    yo
    commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import CustomNavbar from "./components/customNavbar";
 
 function App() {
   return (
     <div className="App">
+      <CustomNavbar />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
