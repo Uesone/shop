ed001e2a6caaf488110ef9718a70d2b24ec7bab7
Umberto Amoroso <34135219+Uesone@users.noreply.github.com>
2024-06-25 17:08:47 +0200

    Update Welcome.jsx

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -4,7 +4,7 @@ import { Alert, Container } from 'react-bootstrap';
 const Welcome = () => {
     return (
         <Container className="mt-5">
-            <Alert variant="success" className="text-center">
+            <Alert variant="warning" className="text-center">
                 <h1>Benvenuto nel negozio bello</h1>
             </Alert>
             <h3 className="text-center text-muted">Compra in questo bellissimo shop</h3>
